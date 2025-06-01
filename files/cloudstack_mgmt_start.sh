ededb0933706dffcc3a6d390dde561cfe3fe74b5
Brad House <brad@brad-house.com>
2025-06-01 07:01:37 -0400

    more gracefully bring online management server

---
+++ files/cloudstack_mgmt_start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+# Usage: cloudstack_mgmt_start.sh stop|0:1
+STOP=$1
+MAX_DURATION=300
+DELAY=10
+
+if [ "$STOP" = "1" ] ; then
+  # Make sure mariadb is stopped
+  systemctl stop cloudstack-management
+  sleep 5
+fi
+
+# Start it
+cloudstack-setup-management
+
+start=`date +%s`
+sleep ${DELAY}
+while ! nc -v localhost 8080 < /dev/null > /dev/null ; do
+  if [ ! systemctl is-active cloudstack-management > /dev/null ] ; then
+    echo "Cloudstack Management unexpectedly quit"
+    exit 1
+  fi
+  curr=`date +%s`
+  let "duration = curr - start"
+  if [ $duration -gt $MAX_DURATION ] ; then
+    echo "Took too long to start"
+    exit 1
+  fi
+  sleep ${DELAY}
+done
+
+sleep ${DELAY}
+
+exit 0
